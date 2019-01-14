0c162a3e5a8eedf880b4208d0e53343aa8552c4d
Thimo Labahn <45945736+TLabahn@users.noreply.github.com>
2019-01-14 10:52:10 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include "SDL_functions.h"
+#include "Game_functions.h"
+
+
+
+int main(int argc, char* args[]){
+
+    //Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_KEYDOWN ){
+						if( e.key.keysym.sym == SDLK_ESCAPE ){
+                            quit = true;
+						}
+					}
+				}
+				SDL_BlitSurface( gMouseSurfaces[ MENU_BACKGROUND ], NULL, gScreenSurface, NULL);
+				SDL_UpdateWindowSurface( gWindow );
+			}
+		}
+	}
+	close();
+
+
+
+    return 0;
+}
